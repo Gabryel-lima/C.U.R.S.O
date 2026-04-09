4ebd28dce6ccf19ea7650aecbac9ada768c08d38
vthugodonl-eng <vthugodon@gmail.com>
2026-04-09 19:37:59 -0300

    Vê se foi

--- index.mjs
+++ index.mjs
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { config } from 'dotenv';
 import cidadaoRota from './src/routes/cidadao.js';
+import equipeptcRota from './src/routes/equipeptc.js';
 
 config();
 
@@ -13,6 +14,8 @@ const port = process.env.API_PORT;
 api.use(bodyParser.json()); 
 
 api.use(cidadaoRota);
+
+api.use(equipeptcRota);
 
 api.get("/teste", async(req, res) => {
     res.status(200).json({Resposta:"Rota de teste implementada"}).end()
---
+++ src/routes/equipeptc.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const roteador = express.Router();
+
+roteador.get('/equipeptc', async(req, res) => {
+    try{
+        res.status(200).json({equipeptc:"Dados da Equipe de Protecao"}).end()
+    }catch(error){
+        res.status(400).json({error:"Error ao consultar Equipe de Protecao"})
+    }
+
+})
+
+export default roteador;
